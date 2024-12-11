cf7cd3f53bb90e54a23e0e2e01571bce0c3d177f
Curry-FishBalls <2359161706@qq.com>
2024-12-11 11:18:06 +0800

    LUI & AUIPC

--- RISC-V/RISC-V.srcs/sources_1/new/ALU.v
+++ RISC-V/RISC-V.srcs/sources_1/new/ALU.v
@@ -1,48 +1,54 @@
+`define ADD 5'd1    //操作数相加
+`define SUB 5'd2    //操作数相减
+`define XOR 5'd3    //按位异或
+`define OR 5'd4     //按位或
+`define AND 5'd5    //按位与
+`define SLL 5'd6    //左移位
+`define SRL 5'd7    //逻辑右移
+`define SRA 5'd8    //算术右移
+`define SLT 5'd9    //有符号比较
+`define SLTU 5'd10  //无符号比较
+`define BEQ 5'd11   //等于跳转
+`define BNE 5'd12   //不等于跳转
+`define BLT 5'd13   //有符号小于跳转
+`define BGE 5'd14   //有符号不小于跳转
+`define BLTU 5'd15  //无符号小于跳转
+`define BGEU 5'd16  //无符号不小于跳转
+`define PC 5'd17    //操作pc
+`define LUI 5'd18   //立即数高位加载
+`define AUIPC 5'd19 //立即数与pc相加
+
 module ALU (
-    input [3:0] ALU_ctrl,
+    input [4:0] ALU_ctrl,
     input [12:0] pc_in,
     input [31:0] num1,
     input [31:0] num2,
     output reg [31:0] ALU_out,
     output reg jump
 );
-    parameter ADD = 5'd1;  //操作数相加
-    parameter SUB = 5'd2;  //操作数相减
-    parameter XOR = 5'd3;  //按位异或
-    parameter OR = 5'd4;  //按位或
-    parameter AND = 5'd5;  //按位与
-    parameter SLL = 5'd6;  //左移位
-    parameter SRL = 5'd7;  //逻辑右移
-    parameter SRA = 5'd8;  //算术右移
-    parameter SLT = 5'd9;  //有符号比较
-    parameter SLTU = 5'd10;  //无符号比较
-    parameter BEQ = 5'd11;  //等于跳转
-    parameter BNE = 5'd12;  //不等于跳转
-    parameter BLT = 5'd13;  //有符号小于跳转
-    parameter BGE = 5'd14;  //有符号不小于跳转
-    parameter BLTU = 5'd15;  //无符号小于跳转
-    parameter BGEU = 5'd16;  //无符号不小于跳转
-    parameter PC = 5'd17;  //操作pc
+
 
     always @(*) begin
         case (ALU_ctrl)
-            ADD:  ALU_out = num1 + num2;
-            SUB:  ALU_out = num1 - num2;
-            XOR:  ALU_out = num1 ^ num2;
-            OR:   ALU_out = num1 | num2;
-            AND:  ALU_out = num1 & num2;
-            SLL:  ALU_out = num1 << num2;
-            SRL:  ALU_out = num1 >> num2;
-            SRA:  ALU_out = num1 >>> num2;
-            SLT:  ALU_out = ($signed(num1) < $signed(num2)) ? 32'd1 : 32'd0;
-            SLTU: ALU_out = (num1 < num2) ? 1 : 0;
-            BEQ:  jump = (num1 == num2);
-            BNE:  jump = (num1 != num2);
-            BLT:  jump = ($signed(num1) < $signed(num2));
-            BGE:  jump = ($signed(num1) >= $signed(num2));
-            BLTU: jump = (num1 < num2);
-            BGEU: jump = (num1 >= num2);
-            PC:   ALU_out = pc_in + 4;
+            `ADD: ALU_out = num1 + num2;
+            `SUB: ALU_out = num1 - num2;
+            `XOR: ALU_out = num1 ^ num2;
+            `OR: ALU_out = num1 | num2;
+            `AND: ALU_out = num1 & num2;
+            `SLL: ALU_out = num1 << num2;
+            `SRL: ALU_out = num1 >> num2;
+            `SRA: ALU_out = num1 >>> num2;
+            `SLT: ALU_out = ($signed(num1) < $signed(num2)) ? 32'd1 : 32'd0;
+            `SLTU: ALU_out = (num1 < num2) ? 1 : 0;
+            `BEQ: jump = (num1 == num2);
+            `BNE: jump = (num1 != num2);
+            `BLT: jump = ($signed(num1) < $signed(num2));
+            `BGE: jump = ($signed(num1) >= $signed(num2));
+            `BLTU: jump = (num1 < num2);
+            `BGEU: jump = (num1 >= num2);
+            `PC: ALU_out = pc_in + 4;
+            `LUI: ALU_out = num2;
+            `AUIPC: ALU_out = pc_in + num2;
             default: begin
                 ALU_out = 0;
                 jump = 0;
--- RISC-V/RISC-V.srcs/sources_1/new/ALU_Control.v
+++ RISC-V/RISC-V.srcs/sources_1/new/ALU_Control.v
@@ -1,55 +1,61 @@
+`define ADD 5'd1    //操作数相加
+`define SUB 5'd2    //操作数相减
+`define XOR 5'd3    //按位异或
+`define OR 5'd4     //按位或
+`define AND 5'd5    //按位与
+`define SLL 5'd6    //左移位
+`define SRL 5'd7    //逻辑右移
+`define SRA 5'd8    //算术右移
+`define SLT 5'd9    //有符号比较
+`define SLTU 5'd10  //无符号比较
+`define BEQ 5'd11   //等于跳转
+`define BNE 5'd12   //不等于跳转
+`define BLT 5'd13   //有符号小于跳转
+`define BGE 5'd14   //有符号不小于跳转
+`define BLTU 5'd15  //无符号小于跳转
+`define BGEU 5'd16  //无符号不小于跳转
+`define PC 5'd17    //操作pc
+`define LUI 5'd18   //立即数高位加载
+`define AUIPC 5'd19 //立即数与pc相加
+
 module ALU_Control (
     input [2:0] func3,
     input func7,
-    input [1:0] aluop,
+    input [2:0] aluop,
     output reg [4:0] ALU_ctrl
 );
-    parameter ADD = 5'd1;  //操作数相加
-    parameter SUB = 5'd2;  //操作数相减
-    parameter XOR = 5'd3;  //按位异或
-    parameter OR = 5'd4;  //按位或
-    parameter AND = 5'd5;  //按位与
-    parameter SLL = 5'd6;  //左移位
-    parameter SRL = 5'd7;  //逻辑右移
-    parameter SRA = 5'd8;  //算术右移
-    parameter SLT = 5'd9;  //有符号比较
-    parameter SLTU = 5'd10;  //无符号比较
-    parameter BEQ = 5'd11;  //等于跳转
-    parameter BNE = 5'd12;  //不等于跳转
-    parameter BLT = 5'd13;  //有符号小于跳转
-    parameter BGE = 5'd14;  //有符号不小于跳转
-    parameter BLTU = 5'd15;  //无符号小于跳转
-    parameter BGEU = 5'd16;  //无符号不小于跳转
-    parameter PC = 5'd17;  //操作pc
+
 
     always @(*) begin
         casez ({
             aluop, func3
         })
-            5'b10_000: begin
-                if (!func7) ALU_ctrl = ADD;
-                else ALU_ctrl = SUB;
+            6'b010_000: begin
+                if (!func7) ALU_ctrl = `ADD;
+                else ALU_ctrl = `SUB;
             end
-            5'b00_???: ALU_ctrl = ADD;
-            5'b10_100: ALU_ctrl = XOR;
-            5'b10_110: ALU_ctrl = OR;
-            5'b10_111: ALU_ctrl = AND;
-            5'b10_100: ALU_ctrl = XOR;
-            5'b10_001: ALU_ctrl = SLL;
-            5'b10_101: begin
-                if (!func7) ALU_ctrl = SRL;
-                else ALU_ctrl = SRA;
+            6'b000_???: ALU_ctrl = `ADD;
+            6'b010_100: ALU_ctrl = `XOR;
+            6'b010_110: ALU_ctrl = `OR;
+            6'b010_111: ALU_ctrl = `AND;
+            6'b010_100: ALU_ctrl = `XOR;
+            6'b010_001: ALU_ctrl = `SLL;
+            6'b010_101: begin
+                if (!func7) ALU_ctrl = `SRL;
+                else ALU_ctrl = `SRA;
             end
-            5'b10_010: ALU_ctrl = SLT;
-            5'b10_011: ALU_ctrl = SLTU;
-            5'b01_000: ALU_ctrl = BEQ;
-            5'b01_001: ALU_ctrl = BNE;
-            5'b01_100: ALU_ctrl = BLT;
-            5'b01_101: ALU_ctrl = BGE;
-            5'b01_110: ALU_ctrl = BLTU;
-            5'b01_111: ALU_ctrl = BGEU;
-            5'b11_???: ALU_ctrl = PC;
-            default:   ALU_ctrl = 5'd0;
+            6'b010_010: ALU_ctrl = `SLT;
+            6'b010_011: ALU_ctrl = `SLTU;
+            6'b001_000: ALU_ctrl = `BEQ;
+            6'b001_001: ALU_ctrl = `BNE;
+            6'b001_100: ALU_ctrl = `BLT;
+            6'b001_101: ALU_ctrl = `BGE;
+            6'b001_110: ALU_ctrl = `BLTU;
+            6'b001_111: ALU_ctrl = `BGEU;
+            6'b011_???: ALU_ctrl = `PC;
+            6'b100_???: ALU_ctrl = `LUI;
+            6'b110_???: ALU_ctrl = `AUIPC;
+            default: ALU_ctrl = 6'd0;
         endcase
     end
 
